4deee56f8f3112e31f35bec83f761afe2db39d9a
Foxflux <43007266+kevinjplee@users.noreply.github.com>
2019-08-02 10:00:42 +0900

    Delete server.sql
    Delete unncessary

--- server.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `school` DEFAULT CHARACTER SET utf8;
-USE `school`;
-
-CREATE TABLE IF NOT EXISTS `accounts` (
-`id` int(11) NOT NULL AUTO_INCREMENT,
-`email` varchar(100) NOT NULL,
-`password` varchar(200) NOT NULL,
-PRIMARY KEY(`id`)
-);
